6e547b88f4d6c91a54afe9f8e5a9296ebfb7d786
ASUS <m>
2021-06-30 07:23:35 +0200

    Up button implemented in action bar.

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +28,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    /**
+     * This method is called whenever the user chooses to navigate Up within your application's
+     * activity hierarchy from the action bar.
+     *
+     *
+     * If a parent was specified in the manifest for this activity or an activity-alias to it,
+     * default Up navigation will be handled automatically. See
+     * [.getSupportParentActivityIntent] for how to specify the parent. If any activity
+     * along the parent chain requires extra Intent arguments, the Activity subclass
+     * should override the method [.onPrepareSupportNavigateUpTaskStack]
+     * to supply those arguments.
+     *
+     *
+     * See [Tasks and
+ * Back Stack]({@docRoot}guide/topics/fundamentals/tasks-and-back-stack.html) from the developer guide and
+     * [Navigation]({@docRoot}design/patterns/navigation.html) from the design guide
+     * for more information about navigating within your app.
+     *
+     *
+     * See the [androidx.core.app.TaskStackBuilder] class and the Activity methods
+     * [.getSupportParentActivityIntent], [.supportShouldUpRecreateTask], and
+     * [.supportNavigateUpTo] for help implementing custom Up navigation.
+     *
+     * @return true if Up navigation completed successfully and this Activity was finished,
+     * false otherwise.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
