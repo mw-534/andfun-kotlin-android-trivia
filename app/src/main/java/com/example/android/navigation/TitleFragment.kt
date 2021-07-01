e0170304b88658ff00a3387046abc97140eafdfb
ASUS <m>
2021-07-01 06:50:55 +0200

    Options menu added which navigates to AboutFragment. Made it appear in action bar only if title screen is shown by adding its setHasOptionsMenu(), onCreateOptionsMenu() and onOptionsItemSelected() methods to TitleFragment.kt instead of its hosting activity.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,14 +1,13 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -32,7 +31,57 @@ class TitleFragment : Fragment() {
             // Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         }
 
+        // Enable Options menu in action bar.
+        // Putting this line of code in a fragment instead of an activity
+        // has the effect of the options menu only appearing when the
+        // activity contains this fragment.
+        setHasOptionsMenu(true)
+
         // return inflated view of fragment
         return binding.root
+    }
+
+    /**
+     * Initialize the contents of the Fragment host's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.  For this method
+     * to be called, you must have first called [.setHasOptionsMenu].  See
+     * [Activity.onCreateOptionsMenu]
+     * for more information.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @see .setHasOptionsMenu
+     *
+     * @see .onPrepareOptionsMenu
+     *
+     * @see .onOptionsItemSelected
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     *
+     *
+     * Derived classes should call through to the base class for it to
+     * perform the default menu handling.
+     *
+     * @param item The menu item that was selected.
+     *
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     *
+     * @see .onCreateOptionsMenu
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
