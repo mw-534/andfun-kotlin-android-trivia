e91b69167b579056a32e7d4a56b3b0d76995d405
ASUS <m>
2021-07-02 05:52:45 +0200

    Implemented navigation safe args plugin to pass arguments safely between fragments. Added parameters to GameWonFragment. Pass arguments to GameWonFragment in GameFragment using the xxxDirections classes which are auto generated by safe args plugin.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -37,6 +38,11 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context,
+            "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
+            Toast.LENGTH_LONG).show()
 
         return binding.root
     }
