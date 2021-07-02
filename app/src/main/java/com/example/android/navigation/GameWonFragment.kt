78d86073096de5756ca3fa4e88da9efc92658154
ASUS <m>
2021-07-02 07:34:44 +0200

    Added share menu item to GameWonFragment and implemented an Intent for sharing the result.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,11 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -39,11 +39,96 @@ class GameWonFragment : Fragment() {
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
 
+        // Show a Toast with the game results which were passed using safe-args plugin.
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
         Toast.makeText(context,
             "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
             Toast.LENGTH_LONG).show()
 
+        // Declaring that our Fragment has a Menu.
+        // This allows onCreateOptionsMenu to be called.
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    /**
+     * Creating share Intent.
+     */
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+
+        /* standard way for intents:
+        val sharedIntent = Intent(Intent.ACTION_SEND)
+        sharedIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+        return sharedIntent
+         */
+
+        // Fluent API for creating sharing intents:
+        return ShareCompat.IntentBuilder.from(activity!!)
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setType("text/plain")
+            .intent
+    }
+
+    /**
+     * Start an Activity with share Intent.
+     */
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    /**
+     * Initialize the contents of the Fragment host's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.  For this method
+     * to be called, you must have first called [.setHasOptionsMenu].  See
+     * [Activity.onCreateOptionsMenu]
+     * for more information.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @see .setHasOptionsMenu
+     *
+     * @see .onPrepareOptionsMenu
+     *
+     * @see .onOptionsItemSelected
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        // Showing the share menu item dynamically:
+        // Check if the activity resolves.
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // Hide the menu item if it doesn't resolve.
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     *
+     *
+     * Derived classes should call through to the base class for it to
+     * perform the default menu handling.
+     *
+     * @param item The menu item that was selected.
+     *
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     *
+     * @see .onCreateOptionsMenu
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // Sharing from the Menu.
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
